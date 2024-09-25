1b9778756a0780d2a9b237bbc9374a9d52798167
Adithya Aravind <adithya@Adithyas-MacBook-Pro.local>
2024-09-25 15:06:10 -0400

    rft: refactoring assignment 2 with java docs

--- assignments/assignment2/src/polynomial/SimplePolynomial.java
+++ assignments/assignment2/src/polynomial/SimplePolynomial.java
@@ -3,11 +3,41 @@ package polynomial;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SimplePolynomial implements Polynomial{
+/**
+ * This class implements the Polynomial interface providing implementation
+ * to add, multiply, and differentiate polynomials.
+ * Is a simple polynomial with single variable and integer coefficients.
+ */
+public class SimplePolynomial implements Polynomial {
 
+
+  /**
+   * A string representation of an empty polynomial.
+   */
+  private static final String EMPTY_POLYNOMIAL = "0";
+
+  /**
+   * A string representation of the plus symbol.
+   */
+  private static final String PLUS_SYMBOL = "+";
+
+  /**
+   * A string representation of the x^ symbol.
+   */
+  private static final String X_POWER_SYMBOL = "x^";
+
+
+  /**
+   * A list to store the coefficients of the polynomial.
+   * The index of the element represents the power of the term.
+   */
   private final List<Integer> polynomialCoefficients;
 
-  public SimplePolynomial(){
+
+  /**
+   * Constructs a SimplePolynomial object with an empty array list.
+   */
+  public SimplePolynomial() {
     polynomialCoefficients = new ArrayList<>();
   }
 
@@ -16,9 +46,11 @@ public class SimplePolynomial implements Polynomial{
   public Polynomial add(Polynomial other) {
     Polynomial resultAfterAdding = new SimplePolynomial();
     int maxDegree = Math.max(this.getDegree(), other.getDegree());
-    for(int i=0;i<=maxDegree;i++){
-      int sum = this.getCoefficient(i) + other.getCoefficient(i);
-      resultAfterAdding.addTerm(sum, i);
+
+    // Add the coefficients of the same power terms from both polynomials.
+    for (int power = 0; power <= maxDegree; power++) {
+      int sum = this.getCoefficient(power) + other.getCoefficient(power);
+      resultAfterAdding.addTerm(sum, power);
     }
     return resultAfterAdding;
   }
@@ -26,10 +58,12 @@ public class SimplePolynomial implements Polynomial{
   @Override
   public Polynomial multiply(Polynomial other) {
     Polynomial resultAfterMultiplying = new SimplePolynomial();
-    for(int i=0;i<=this.getDegree();i++){
-      for(int j=0;j<=other.getDegree();j++){
+
+    // Multiply the coefficients of the terms from both polynomials.
+    for (int i = 0; i <= this.getDegree(); i++) {
+      for (int j = 0; j <= other.getDegree(); j++) {
         int product = this.getCoefficient(i) * other.getCoefficient(j);
-        resultAfterMultiplying.addTerm(product, i+j);
+        resultAfterMultiplying.addTerm(product, i + j);
       }
     }
     return resultAfterMultiplying;
@@ -37,38 +71,27 @@ public class SimplePolynomial implements Polynomial{
 
   @Override
   public Polynomial derivative() {
-    // TODO: recheck this
     Polynomial resultAfterDerivative = new SimplePolynomial();
-    for(int i=1;i<=this.getDegree();i++){
-      int derivativeCoefficient = this.getCoefficient(i) * i;
-      resultAfterDerivative.addTerm(derivativeCoefficient, i-1);
+
+    // Ignoring the first derivative since derivative of a constant is 0.
+    for (int power = 1; power <= this.getDegree(); power++) {
+      int derivativeCoefficient = this.getCoefficient(power) * power;
+      resultAfterDerivative.addTerm(derivativeCoefficient, power - 1);
     }
     return resultAfterDerivative;
   }
 
   @Override
   public void addTerm(int coefficient, int power) throws IllegalArgumentException {
-    if(power<0){
-      throw new IllegalArgumentException("Power cannot be negative");
-    }
-
-    if(power==polynomialCoefficients.size()) {
-      polynomialCoefficients.add(coefficient);
-    }
-    else if(power< polynomialCoefficients.size()){
-      polynomialCoefficients.set(power, polynomialCoefficients.get(power)+coefficient);
-    }
-    else{
-      for(int i= polynomialCoefficients.size();i<power;i++){
-        polynomialCoefficients.add(0);
-      }
-      polynomialCoefficients.add(coefficient);
-    }
+    addCoefficientToAppropriateIndex(coefficient, power);
+    // Cleaning up trailing zeroes is necessary to ensure that maximum degree is the last element.
+    cleanUpTrailingZeros();
   }
 
   @Override
   public int getDegree() {
-    if (polynomialCoefficients.isEmpty()) {
+    // Returns 0 if the polynomial is empty.
+    if (isPolynomialEmpty()) {
       return 0;
     }
     return polynomialCoefficients.size() - 1;
@@ -77,59 +100,141 @@ public class SimplePolynomial implements Polynomial{
   @Override
   public double evaluate(double x) {
     double evaluatedValue = 0;
-    for (int i = 0; i <= this.getDegree(); i++) {
-      evaluatedValue += polynomialCoefficients.get(i) * Math.pow(x, i);
+
+    for (int power = 0; power <= this.getDegree(); power++) {
+      evaluatedValue += this.getCoefficient(power) * Math.pow(x, power);
     }
     return evaluatedValue;
   }
 
   @Override
   public int getCoefficient(int power) {
-    if(power<0 || power> this.getDegree()){
+
+    // If power is out of bounds, returns 0.
+    if (power < 0 || power > this.getDegree()) {
       return 0;
     }
     return polynomialCoefficients.get(power);
   }
 
+  /**
+   * Compares simple polynomial with another object to verify if they are equal or not.
+   * If the object is not an instance of SimplePolynomial then returns false.
+   * Additional checks are performed to ensure polynomial coefficients are equal.
+   *
+   * @param obj the object to compare with
+   * @return true if the objects are equal, false otherwise
+   */
   @Override
   public boolean equals(Object obj) {
-    // TODO: recheck this
     if (this == obj) {
       return true;
     }
-    if (obj == null || getClass() != obj.getClass()) {
+    if (!(obj instanceof SimplePolynomial)) {
       return false;
     }
     SimplePolynomial that = (SimplePolynomial) obj;
     return polynomialCoefficients.equals(that.polynomialCoefficients);
   }
 
+  /**
+   * Generates a hash code using coefficients of the simple polynomial.
+   *
+   * @return the hash code for the simple polynomial
+   */
   @Override
   public int hashCode() {
-    // TODO: recheck this
     return polynomialCoefficients.hashCode();
   }
 
   @Override
-  public String toString(){
-    if(polynomialCoefficients.isEmpty()){
-      return "0";
+  public String toString() {
+    if (isPolynomialEmpty()) {
+      return EMPTY_POLYNOMIAL;
     }
     StringBuilder polynomialAsString = new StringBuilder();
-    for (int i =this.getDegree();i>=0;i--){
-      int coefficient = polynomialCoefficients.get(i);
-      if(coefficient==0){
+    int currentDegree = this.getDegree();
+
+    for (int i = currentDegree; i >= 0; i--) {
+      int coefficient = this.getCoefficient(i);
+
+      if (coefficient == 0) {
         continue;
       }
-      if(coefficient>0 && i!=this.getDegree()){
-        polynomialAsString.append("+");
+
+      if (coefficient > 0 && i != currentDegree) {
+        polynomialAsString.append(PLUS_SYMBOL);
       }
 
       polynomialAsString.append(coefficient);
-      if(i>0){
-        polynomialAsString.append("x^").append(i);
+
+      if (i > 0) {
+        polynomialAsString.append(X_POWER_SYMBOL).append(i);
       }
     }
+
     return polynomialAsString.toString();
   }
+
+  /**
+   * Checks if the polynomial is empty.
+   *
+   * @return true if the polynomial is empty, false otherwise
+   */
+  private boolean isPolynomialEmpty() {
+    return polynomialCoefficients.isEmpty();
+  }
+
+  /**
+   * Removes trailing zeros from the polynomial by checking
+   * the coefficients from the highest power to the lowest until a non-zero
+   * coefficient is encountered.
+   */
+  private void cleanUpTrailingZeros() {
+    int power = this.getDegree();
+    while (!isPolynomialEmpty() && power >= 0 && this.getCoefficient(power) == 0) {
+      polynomialCoefficients.remove(power);
+      power--;
+    }
+  }
+
+  /**
+   * Validates the power of the term to be added.
+   *
+   * @param power power of the term to be added
+   * @throws IllegalArgumentException if the power is negative
+   */
+  private void validatePower(int power) throws IllegalArgumentException {
+    if (power < 0) {
+      throw new IllegalArgumentException(
+              String.format("Power cannot be negative. Received: %d", power));
+    }
+  }
+
+  /**
+   * Adds the coefficient to the appropriate index in the polynomial.
+   * If the coefficient is 0, it is not added to the polynomial.
+   *
+   * @param coefficient the coefficient of the term to be added
+   * @param power       the power of the term to be added
+   * @throws IllegalArgumentException if power fails validation
+   */
+  private void addCoefficientToAppropriateIndex(
+          int coefficient, int power) throws IllegalArgumentException {
+    // Validation is necessary before adding the polynomial term.
+    validatePower(power);
+    if (coefficient == 0) {
+      return;
+    }
+    if (power < polynomialCoefficients.size()) {
+      polynomialCoefficients.set(power, this.getCoefficient(power) + coefficient);
+    } else {
+      // Defaults to add 0 as coefficients for missing powers.
+      for (int i = polynomialCoefficients.size(); i < power; i++) {
+        polynomialCoefficients.add(0);
+      }
+      polynomialCoefficients.add(coefficient);
+    }
+  }
+
 }
